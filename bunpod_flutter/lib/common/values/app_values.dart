aec738c9afb10221d9dc43813205898f409f1207
kamranbekirovyz <kamranbekirovyz@gmail.com>
2026-07-04 20:37:01 +0400

    feat: add maker contact values

--- bunpod_flutter/lib/common/values/app_values.dart
+++ bunpod_flutter/lib/common/values/app_values.dart
@@ -1,3 +1,10 @@
 abstract final class AppValues {
   static String get title => 'BunPod';
+  static String get makerImageUrl =>
+      'https://avatars.githubusercontent.com/u/59581562?v=4';
+  static String get makerName => 'Kamran Bekirov';
+  static String get makerXHandle => '@kamranbekirovyz';
+  static String get makerXUrl => 'https://x.com/kamranbekirovyz';
+  static String get makerPortfolioUrl => 'https://kamranbekirov.com';
+  static String get makerEmail => 'me@kamranbekirov.com';
 }
