9f0f669958e5b5516e660b63a24c6e063c1531f2
kamranbekirovyz <kamranbekirovyz@gmail.com>
2026-07-04 20:59:37 +0400

    feat: wire theme mode into app

--- bunpod_flutter/lib/app.dart
+++ bunpod_flutter/lib/app.dart
@@ -1,30 +1,38 @@
 import 'package:bunpod_flutter/bunpod_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppValues.title,
-      debugShowCheckedModeBanner: false,
-      themeMode: .dark,
-      theme: MaterialThemes.light,
-      darkTheme: MaterialThemes.dark,
-      builder: (context, child) {
-        final MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return BlocBuilder<ThemeModeCubit, ThemeMode>(
+      bloc: locator<ThemeModeCubit>(),
+      builder: (context, state) {
+        final ThemeMode themeMode = state;
 
-        return MediaQuery(
-          data: mediaQueryData.copyWith(
-            textScaler: mediaQueryData.textScaler.clamp(
-              maxScaleFactor: 1.1,
-            ),
-          ),
-          child: child!,
+        return MaterialApp(
+          title: AppValues.title,
+          debugShowCheckedModeBanner: false,
+          themeMode: themeMode,
+          theme: MaterialThemes.light,
+          darkTheme: MaterialThemes.dark,
+          builder: (context, child) {
+            final MediaQueryData mediaQueryData = MediaQuery.of(context);
+
+            return MediaQuery(
+              data: mediaQueryData.copyWith(
+                textScaler: mediaQueryData.textScaler.clamp(
+                  maxScaleFactor: 1.1,
+                ),
+              ),
+              child: child!,
+            );
+          },
+          home: const WelcomePage(),
         );
       },
-      home: const WelcomePage(),
     );
   }
 }
