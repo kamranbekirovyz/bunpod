f461bd5fd8df76f6f0687750c0389a07a1c4c1a4
kamranbekirovyz <kamranbekirovyz@gmail.com>
2026-07-18 01:14:44 +0400

    refactor: renamed home player card

--- bunpod_flutter/lib/features/home/home.dart
+++ bunpod_flutter/lib/features/home/home.dart
@@ -4,5 +4,5 @@ export 'values/mock_values.dart';
 export 'widgets/episode_card.dart';
 export 'widgets/filter_tabs.dart';
 export 'widgets/home_app_bar.dart';
-export 'widgets/player_card.dart';
+export 'widgets/home_player_card.dart';
 export 'widgets/section_header.dart';
